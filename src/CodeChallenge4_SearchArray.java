1c8edcf6d0b98203050e518ff3c51951b282ce1e
joice <joydingforever@gmail.com>
2022-07-17 14:52:44 -0700

    Initial commit

---
+++ src/CodeChallenge4_SearchArray.java
@@ -0,0 +1,11 @@
+
+class CodeChallenge4_SearchArray {
+    public static void main(String[] args) {
+
+    /*
+    Create an array of strings. Search through it to so prints the the correct word.
+    */
+        String[] Y = {"wrong", "wrong", "correct", "wrong"};
+        System.out.println(Y[2]);
+    }
+}
